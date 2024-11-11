aef36cc70fcacb34c20d11e90c4d6344a724811e
Beatriz862 <ana.00000850308@unicap.br>
2024-11-11 13:16:41 -0300

    Add Biblioteca e atualizando CouserList

--- src/components/Course/CouserList.js
+++ src/components/Course/CouserList.js
@@ -2,23 +2,61 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import CourseCard from './CourseCard';
 
-const CourseList = ({ courses }) => {
+const CourseList = () => {
+  const courses = [
+    {
+      id: 1,
+      title: 'Educação Financeira',
+      image: 'https://via.placeholder.com/150?text=Educação+Financeira', 
+    },
+    {
+      id: 2,
+      title: 'Desenvolvimento Pessoal',
+      image: 'https://via.placeholder.com/150?text=Desenvolvimento+Pessoal',
+    },
+    {
+      id: 3,
+      title: 'Empreendedorismo',
+      image: 'https://via.placeholder.com/150?text=Empreendedorismo', 
+    },
+    {
+      id: 4,
+      title: 'Autoconhecimento',
+      image: 'https://via.placeholder.com/150?text=Autoconhecimento',
+    },
+    {
+      id: 5,
+      title: 'Educação Financeira',
+      image: 'https://via.placeholder.com/150?text=Educação+Financeira',
+    },
+    {
+      id: 6,
+      title: 'Desenvolvimento Pessoal',
+      image: 'https://via.placeholder.com/150?text=Desenvolvimento+Pessoal',
+    },
+    {
+      id: 7,
+      title: 'Empreendedorismo',
+      image: 'https://via.placeholder.com/150?text=Empreendedorismo', 
+    },
+    {
+      id: 8,
+      title: 'Autoconhecimento',
+      image: 'https://via.placeholder.com/150?text=Autoconhecimento', 
+    },
+  ];
+
   return (
-    <>
-      {courses.map((courseGroup, index) => (
-        <div key={index}>
-          <h2 style={{ marginBottom: '16px' }}>{courseGroup.category}</h2>
-          <Row gutter={[16, 16]}>
-            {courseGroup.data.map((course, i) => (
-              <Col key={i} xs={24} sm={12} md={8} lg={6}>
-                <CourseCard course={course} />
-              </Col>
-            ))}
-          </Row>
-        </div>
+    <Row gutter={[16, 16]}>
+      {courses.map((course) => (
+        <Col key={course.id} xs={24} sm={12} md={8} lg={6}>
+          <CourseCard course={course} />
+        </Col>
       ))}
-    </>
+    </Row>
   );
 };
 
 export default CourseList;
+
+
